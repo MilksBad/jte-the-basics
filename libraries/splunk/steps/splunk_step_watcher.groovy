160e437de716384a8b3eb774bd0534a40eb64188
MilksBad <Kirk_Ryn@bah.com>
2021-03-17 00:08:51 -0400

    1

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,11 +3,11 @@ void before(){
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
-@AfterStep
-void after(){
-    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
-}
+//@AfterStep
+//void after(){
+//    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
+//}
 
 @AfterStep({ hookContext.step.equals("static_code_analysis") })
 
-@AfterStep({ hookContext.step in config.afterSteps })
+//@AfterStep({ hookContext.step in config.afterSteps })
